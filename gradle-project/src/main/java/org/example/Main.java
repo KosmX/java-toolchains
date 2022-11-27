1ed7fb8440a517b0015edd7210390336e8ab7224
KosmX <kosmx.mc@gmail.com>
2022-11-27 14:51:52 +0100

    create solution

--- gradle-project/src/main/java/org/example/Main.java
+++ gradle-project/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-//import io.github.kosmx.edu.math.vector3.Vector3D;
+import io.github.kosmx.edu.math.vector3.Vector3D;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,11 +11,10 @@ public class Main {
         JsonObject obj = JsonParser.parseString("{\"str\": \"Hello gson!\"}").getAsJsonObject();
         System.out.println(obj.get("str").getAsString());
 
-        /* // Vector test
+        // Vector test
         Vector3D a = new Vector3D(4, 0, 0);
         Vector3D b = new Vector3D(0, 5, 0);
         System.out.println(a.cross(b).length());
-        */
 
     }
 }
--- maven-project/src/main/java/org/example/Main.java
+++ maven-project/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-//import io.github.kosmx.edu.math.vector3.Vector3D;
+import io.github.kosmx.edu.math.vector3.Vector3D;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,10 +10,10 @@ public class Main {
         JsonObject obj = JsonParser.parseString("{\"str\": \"Hello gson!\"}").getAsJsonObject();
         System.out.println(obj.get("str").getAsString());
 
-        /* // Vector 3d test code
+        // Vector 3d test code
         Vector3D a = new Vector3D(4, 0, 0);
         Vector3D b = new Vector3D(0, 5, 0);
         System.out.println(a.cross(b).length());
-         */
+
     }
 }
